42f4f03b9a8e4812a7cf38787bdc3c2c8e83cc57
n-hamamoto <n.hamamoto@gunma-u.ac.jp>
2020-03-03 15:00:54 +0900

    Update deploy.sh

--- mkconfig/deploy.sh
+++ mkconfig/deploy.sh
@@ -4,8 +4,9 @@ dst=..
 
 cp docker-compose.yml     $dst
 cp shibboleth2.xml        $dst/proxy/conf/etc/shibboleth
-cp -r metadata            $dst/proxy/conf/etc/shibboleth
+#cp -r metadata            $dst/proxy/conf/etc/shibboleth
 cp -r cert                $dst/proxy/conf/etc/shibboleth
+mkdir -p                  $dst/proxy/conf/etc/pki/ssl
 cp -r cert/*              $dst/proxy/conf/etc/pki/ssl/
 cp proxy.conf             $dst/proxy/conf/etc/httpd/conf.d/
 cp shib.conf              $dst/proxy/conf/etc/httpd/conf.d/
