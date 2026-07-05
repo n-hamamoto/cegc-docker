b761449c615e5203d6314c5ad43dbc195e865ada
n-hamamoto <n.hamamoto@gunma-u.ac.jp>
2026-07-05 18:52:18 +0900

    Update copy-config.sh

--- setup/proxy/copy-config.sh
+++ setup/proxy/copy-config.sh
@@ -3,7 +3,7 @@
 CONFDIR=./sampleconf
 DSTDIR=../../proxy/conf
 
-cp -r ${CONFDIR}/vhost ${DSTDIR}/etc/httpd/conf.d
+#cp -r ${CONFDIR}/vhost ${DSTDIR}/etc/httpd/conf.d
 
 mv    ${DSTDIR}/etc/httpd/conf/httpd.conf ${DSTDIR}/etc/httpd/conf/httpd.conf.dist
 mv    ${DSTDIR}/etc/httpd/conf.d/ssl.conf ${DSTDIR}/etc/httpd/conf.d/ssl.conf.dist
@@ -23,10 +23,10 @@ cp    ${CONFDIR}/shibboleth/native.logger      ${DSTDIR}/etc/shibboleth/
 cp    ${CONFDIR}/shibboleth/shibd.logger       ${DSTDIR}/etc/shibboleth/
 cp    ${CONFDIR}/shibboleth/attribute-map.xml  ${DSTDIR}/etc/shibboleth/
 cp -r ${CONFDIR}/shibboleth/cert               ${DSTDIR}/etc/shibboleth/
-cp -r ${CONFDIR}/shibboleth/metadata           ${DSTDIR}/etc/shibboleth/
+#cp -r ${CONFDIR}/shibboleth/metadata           ${DSTDIR}/etc/shibboleth/
 
-cp -r ${CONFDIR}/ssl ${DSTDIR}/etc/pki
-cp -r ${CONFDIR}/vhost  ${DSTDIR}/etc/httpd/conf.d/
+#cp -r ${CONFDIR}/ssl ${DSTDIR}/etc/pki
+#cp -r ${CONFDIR}/vhost  ${DSTDIR}/etc/httpd/conf.d/
 
 cp -r ${CONFDIR}/services.d   ${DSTDIR}/etc/
 cp -r ${CONFDIR}/fix-attrs.d  ${DSTDIR}/etc/
