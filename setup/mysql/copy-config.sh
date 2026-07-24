7f99abd3cdd6dcd379cc48dd5381a4bf3b18b623
root <n.hamamoto@gunma-u.ac.jp>
2026-07-24 16:59:16 +0900

    Add comment in init.sh and omit redundant blank in copy-config.sh

--- setup/mysql/copy-config.sh
+++ setup/mysql/copy-config.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
 CONFDIR=./sampleconf
-DSTDIR=../../mysql/conf/
+DSTDIR=../../mysql/conf
 
 mv    ${DSTDIR}/etc/mysql/conf.d/mysql.cnf ${DSTDIR}/etc/mysql/conf.d/mysql.cnf.dist
-
 cp    ${CONFDIR}/mysql.cnf    ${DSTDIR}/etc/mysql/conf.d/
-
-
